8c5161c5bcc557ae8af8c017242f80855e83328e
Tobias Fenster <tfenster@4psbau.de>
2025-02-22 16:54:20 +0000

    initial

---
+++ bc-client/Program.cs
@@ -0,0 +1,11 @@
+﻿var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("http://wiremock:8080/some/thing"),
+
+};
+using var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+var body = await response.Content.ReadAsStringAsync();
+Console.WriteLine(body);
